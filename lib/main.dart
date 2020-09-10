36a2aa70036e0713c338debd0b65d98e1485b3fa
thehamzarocks <thehamzarocks@gmail.com>
2020-09-10 07:05:03 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Junkaday',
+        theme: ThemeData(primaryColor: Colors.white),
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _savedSuggestions = Set<WordPair>();
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Junk a Day'),
+          actions: [IconButton(icon: Icon(Icons.list), onPressed: _showSaved)],
+        ),
+        body: _buildSuggestions());
+  }
+
+  void _showSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext buildContext) {
+      final tiles = _savedSuggestions.map((WordPair pair) {
+        return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+      });
+      final divided =
+          ListTile.divideTiles(tiles: tiles, context: buildContext).toList();
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Favorited Junk'),
+          ),
+          body: ListView(children: divided));
+    }));
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    final alreadySaved = _savedSuggestions.contains(wordPair);
+    return ListTile(
+      title: Text(wordPair.asPascalCase, style: _biggerFont),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedSuggestions.remove(wordPair);
+          } else {
+            _savedSuggestions.add(wordPair);
+          }
+        });
+      },
+    );
+  }
+}
