b09ed91ac7eec8d83871c4e0513857bdc00c9a39
thehamzarocks <thehamzarocks@gmail.com>
2020-10-11 20:00:11 +0530

    adding milestone 2

---
+++ lib/milestone/mileStoneTextMap.dart
@@ -0,0 +1,14 @@
+final Map<int, Map<String, dynamic>> mileStoneTextMap = {
+  0: {'mainText': 'Is there a ... a wanderer?', 'additionalInfo': ''},
+  1: {
+    'mainText':
+        'Wanderer! Many have ventured through this path, though few have found what they sought. Most return here to the very beginning, often in worse shape. Be wary. Step forth, but step true.',
+    'additionalInfo': 'Consumables shop unlocked.'
+  },
+  2: {
+    'mainText':
+        'I see you have passion wanderer. But danger lurks ahead, many speak of a foul spirit nearby',
+    'additionalInfo':
+        '(Thank you for trying out the beta, more milestones coming soon. Your mints won\'t update until the next update, but keep logging and keep your health up!)'
+  },
+};
